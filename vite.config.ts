352dfceebad8557325741d87c7b3f692633caca3
peng.hao <peng.hao@okg.com>
2025-12-01 21:07:23 +0800

    安装打包体积可视化插件，并配置命令

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { visualizer } from 'rollup-plugin-visualizer' // 分析打包体积
+
+const shouldAnalyze = process.env.ANALYZE === 'true'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,6 +18,18 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        shouldAnalyze && visualizer({
+          filename: 'dist/bundle-report.html', // 报告输出位置
+          gzipSize: true,                      // 显示 gzip 后大小
+          brotliSize: true,                    // 显示 brotli 后大小
+          open: true,                          // 构建完自动打开报告
+        }),
+      ],
     },
   },
 })
